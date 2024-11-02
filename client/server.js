cd8f6663cf8055986235d1486a30cedce0684b24
ab490 <anooshkabajaj@gmail.com>
2024-11-02 18:27:29 -0400

    resolving issue in server.js

--- client/server.js
+++ client/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 const app = express();
 
+const app = express();
 app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
