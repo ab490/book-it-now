815cd0e3f0a52a5c7e78f9c2b38c25826a3769ee
ab490 <anooshkabajaj@gmail.com>
2025-01-08 16:18:15 -0500

    removing debug from dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,20 +1,15 @@
 const Dashboard = () => {
     return (
-        <div>
+        <div style={{ width: '100%', height: '800px' }}>
             <h1>Visualizations Dashboard</h1>
-            <p>If you see this, the component is rendering properly.</p>
+            <iframe
+                title="Power BI Dashboard"
+                width="100%"
+                height="100%"
+                src="https://app.powerbi.com/groups/me/reports/419c49b4-9541-4fad-b7c4-afa008d29fd4/74e10b007580064071e9?experience=power-bi"
+                allowFullScreen={true}
+            ></iframe>
         </div>
-
-        // <div style={{ width: '100%', height: '800px' }}>
-        //     <h1>Visualizations Dashboard</h1>
-        //     <iframe
-        //         title="Power BI Dashboard"
-        //         width="100%"
-        //         height="100%"
-        //         src="https://app.powerbi.com/groups/me/reports/419c49b4-9541-4fad-b7c4-afa008d29fd4/74e10b007580064071e9?experience=power-bi"
-        //         allowFullScreen={true}
-        //     ></iframe>
-        // </div>
     );
 };
 
