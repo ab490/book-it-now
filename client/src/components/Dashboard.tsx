1e4d5347b5efa575a85b146a7d71d10afafa8468
ab490 <anooshkabajaj@gmail.com>
2025-01-08 16:10:11 -0500

    debugging dashboard component

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,15 +1,20 @@
 const Dashboard = () => {
     return (
-        <div style={{ width: '100%', height: '800px' }}>
+        <div>
             <h1>Visualizations Dashboard</h1>
-            <iframe
-                title="Power BI Dashboard"
-                width="100%"
-                height="100%"
-                src="https://app.powerbi.com/groups/me/reports/419c49b4-9541-4fad-b7c4-afa008d29fd4/74e10b007580064071e9?experience=power-bi"
-                allowFullScreen={true}
-            ></iframe>
+            <p>If you see this, the component is rendering properly.</p>
         </div>
+
+        // <div style={{ width: '100%', height: '800px' }}>
+        //     <h1>Visualizations Dashboard</h1>
+        //     <iframe
+        //         title="Power BI Dashboard"
+        //         width="100%"
+        //         height="100%"
+        //         src="https://app.powerbi.com/groups/me/reports/419c49b4-9541-4fad-b7c4-afa008d29fd4/74e10b007580064071e9?experience=power-bi"
+        //         allowFullScreen={true}
+        //     ></iframe>
+        // </div>
     );
 };
 
