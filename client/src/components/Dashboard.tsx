ae82b579e240a6907f166da29a1a69760ac40e92
ab490 <anooshkabajaj@gmail.com>
2025-01-08 16:28:11 -0500

    dashboard set to public

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -4,9 +4,9 @@ const Dashboard = () => {
             <h1>Visualizations Dashboard</h1>
             <iframe
                 title="Power BI Dashboard"
-                width="1140"
-                height="541.25"
-                src="https://app.powerbi.com/reportEmbed?reportId=419c49b4-9541-4fad-b7c4-afa008d29fd4&autoAuth=true&ctid=1113be34-aed1-4d00-ab4b-cdd02510be91"
+                width="600"
+                height="373.5"
+                src="https://app.powerbi.com/view?r=eyJrIjoiMGYyZGRiMWItMTQwNC00Y2NmLTkyNTctOTcyYjhmOGZkOTIwIiwidCI6IjExMTNiZTM0LWFlZDEtNGQwMC1hYjRiLWNkZDAyNTEwYmU5MSIsImMiOjN9"
                 frameBorder="0"
                 allowFullScreen={true}
                 style={{ border: 'none' }}
