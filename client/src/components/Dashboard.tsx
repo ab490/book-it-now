426d55929162bbf62a561fcf871341d8a2dae90c
ab490 <anooshkabajaj@gmail.com>
2025-01-08 16:23:00 -0500

    corrected powerbi link

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,13 +1,15 @@
 const Dashboard = () => {
     return (
-        <div style={{ width: '100%', height: '800px' }}>
+        <div style={{ width: '100%', textAlign: 'center', marginTop: '20px' }}>
             <h1>Visualizations Dashboard</h1>
             <iframe
                 title="Power BI Dashboard"
-                width="100%"
-                height="100%"
-                src="https://app.powerbi.com/groups/me/reports/419c49b4-9541-4fad-b7c4-afa008d29fd4/74e10b007580064071e9?experience=power-bi"
+                width="1140"
+                height="541.25"
+                src="https://app.powerbi.com/reportEmbed?reportId=419c49b4-9541-4fad-b7c4-afa008d29fd4&autoAuth=true&ctid=1113be34-aed1-4d00-ab4b-cdd02510be91"
+                frameBorder="0"
                 allowFullScreen={true}
+                style={{ border: 'none' }}
             ></iframe>
         </div>
     );
